ce37221cf5a856e1968fc2b2c8b1180f939cb7b1
Simon Larsén <slarse@slar.se>
2022-06-19 14:23:34 +0200

    Document crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! A Python-like programming language implemented completely in Rust.
+//!
+//! Rusthon is a programming language compiler and runtimie for a Python-like
+//! programming language.
+//!
+//! Rusthon is **not meant to ever be a production-ready programming language**.
+//! It is simply a medium for learning Rust and exploring various topics of
+//! programming language design.
+
 pub mod lexer;
 
 pub mod parser;
